23172bedeabbda877e1339566b5b0e281707f980
imp <imp@x1c.0mq.io>
2022-01-21 00:06:11 -0500

    Added merge sort

--- lib/src/test/scala/imp/interview/array/ArrayTests.scala
+++ lib/src/test/scala/imp/interview/array/ArrayTests.scala
@@ -20,4 +20,10 @@ class ArrayTests extends AnyFunSuite {
     val res_immutable_linear = subArraySum.solutionImmutable_Linear(nums,1)
     Assert.assertEquals(8,res_immutable_linear)
   }
+
+  test(testName = "Merge Sort") {
+    val input = Array(8, 3, 5, 4, 6, 1, 0, 2, 7, 9)
+    val mergeSort = new MergeSort;
+    Assert.assertTrue(mergeSort.mergeSort(input).sameElements(Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)))
+  }
 }
